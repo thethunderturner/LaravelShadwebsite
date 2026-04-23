21766cbcfb1eea3cdc2856d2f78aec7b1b205641
Matthew Biskas <matthewbiskas@gmail.com>
2026-04-23 12:40:12 +0200

    fix:broken table

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,3 +1,12 @@
 <x-layout title="Blog Posts" description="Latest news about the shadPS4 emulator">
+    @push('head')
+        @vite('resources/css/filament/shadpanel/theme.css')
+        @filamentStyles
+    @endpush
+
+    @push('scripts')
+        @filamentScripts
+    @endpush
+
     @livewire('grid-posts-table')
 </x-layout>
